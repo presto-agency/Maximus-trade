ba73b57564d49f9b1b399b005c782b1c30d0bedc
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-08-19 16:33:57 +0300

    feat: add js to links

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,5 +1,6 @@
 window.addEventListener('DOMContentLoaded', function () {
     headerActive()
+    addClassToLink()
 })
 
 let headerActive = () => {
@@ -16,6 +17,31 @@ let headerActive = () => {
     }
     headerMob?burgerBtn.onclick = () => {headerMob.classList.contains('active')?removeClass():addClass()}:null;
 }
+
+let addClassToLink = () => {
+    let objects = document.querySelectorAll('.how-to__list>li');
+    let topValue = window.innerHeight / 2;
+    objects.forEach(object => {
+        let bottomValue = (window.innerHeight / 2) - object.offsetHeight;
+        function isFullyVisible(el) {
+            let elementBoundary = el.getBoundingClientRect();
+            let top = elementBoundary.top;
+            let bottom = elementBoundary.bottom;
+            return ((top <= topValue) && (bottom >= bottomValue));
+        }
+
+        function scrolling() {
+            if (isFullyVisible(object) && !object.classList.contains('active')) {
+                object.classList.add('active')
+            }
+            else if (!isFullyVisible(object) && object.classList.contains('active')){
+                object.classList.remove('active');
+            }
+        }
+        window.addEventListener("scroll", scrolling);
+    })
+
+};
 ;
 
 
