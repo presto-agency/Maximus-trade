b34b00677b11859f881b77de85ed39c8e84e2711
Kamarhinanton <Kamarhin_anton@ukr.net>
2022-09-02 12:53:35 +0300

    feat: add validation to forms safety(the same passwords)

--- #src/js/common.js
+++ #src/js/common.js
@@ -22,10 +22,25 @@ let headerActive = () => {
 let validateForm = () => {
     // https://via-profit.github.io/js-form-validator/
     let formsHandle = document.querySelectorAll('form');
+
     formsHandle.forEach( formHandle => {
         const validator = new Validator(formHandle, function (err, res) {
             return res;
-        }, {errorClassName: 'validate-error'})
+        },{
+            rules: {
+                compare: function (value) {
+                    return (value === document.getElementById('input_test').value);
+                }
+            },
+            messages: {
+                en: {
+                    compare: {
+                        incorrect: 'Passwords are not the same'
+                    }
+                }
+            },
+            errorClassName: 'validate-error'
+        })
     })
 }
 
